22740637a144d6836a3a7b739fdf6241e1573688
Kaddu Livingstone <kaddulivingston@gmail.com>
2022-11-07 18:04:37 +0300

    change

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,28 +1,65 @@
-import {object,string,TypeOf } from 'zod'
+import { object, string, TypeOf } from "zod";
 
 export const createUserSchema = object({
-    body:object({
-        firstName:string({
-            required_error:"First name is required"
-        }),
-        lastName:string({
-            required_error:"Last name is required"
-        }),
-        password:string({
-            required_error: "password is required"
-        }).min(6,"Password should be at least 6 characters"),
+  body: object({
+    firstName: string({
+      required_error: "First name is required",
+    }),
+    lastName: string({
+      required_error: "Last name is required",
+    }),
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password is too short - should be min 6 chars"),
+    passwordConfirmation: string({
+      required_error: "Password confirmation is required",
+    }),
+    email: string({
+      required_error: "Email is required",
+    }).email("Not a valid email"),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  }),
+});
 
-        passwordConfirmation:string ({
-            required_error: "Password confirmation is required"
-        }),
-        email:string({
-            required_error:"Email is required"
-        }).email("Please provide a valid email")
+export const verifyUserSchema = object({
+  params: object({
+    id: string(),
+    verificationCode: string(),
+  }),
+});
 
-    }).refine((data)=>data.password == data.passwordConfirmation,{
-        message:"Passwords do not match",
-        path: ["passwordConfirmation"]
-    })
-}) 
+export const forgotPasswordSchema = object({
+  body: object({
+    email: string({
+      required_error: "Email is required",
+    }).email("Not a valid email"),
+  }),
+});
 
-export type CreateUserInput = TypeOf<typeof createUserSchema>['body']
+export const resetPasswordSchema = object({
+  params: object({
+    id: string(),
+    passwordResetCode: string(),
+  }),
+  body: object({
+    password: string({
+      required_error: "Password is required",
+    }).min(6, "Password is too short - should be min 6 chars"),
+    passwordConfirmation: string({
+      required_error: "Password confirmation is required",
+    }),
+  }).refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  }),
+});
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>["body"];
+
+export type VerifyUserInput = TypeOf<typeof verifyUserSchema>["params"];
+
+export type ForgotPasswordInput = TypeOf<typeof forgotPasswordSchema>["body"];
+
+export type ResetPasswordInput = TypeOf<typeof resetPasswordSchema>;
